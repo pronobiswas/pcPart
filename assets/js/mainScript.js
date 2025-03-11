302a8f1ee089bd102f09e50b5e8655cd969bb2e8
PRONOB BISWAS <80696443+pronobiswas@users.noreply.github.com>
2025-03-11 21:27:48 +0600

    first commit

---
+++ assets/js/mainScript.js
@@ -0,0 +1,8 @@
+function openAsideMenu(){
+  const asideMenu =  document.querySelector(".asideMenu");
+  asideMenu.style.display = "block";
+}
+function closeAsideMenu(){
+    const asideMenu =  document.querySelector(".asideMenu");
+  asideMenu.style.display = "none";
+}
